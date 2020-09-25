ad4530d67e92d29eebdac3ae3e34e51fe659b236
James Gaddis <jdgaddis@usf.edu>
2020-09-24 23:51:01 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+    Author: James Gaddis
+    Date: 09/24/2020
+    Comments: This C# console application asks the user to enter their name.
+              Then using a method to print a message that says "Hello user's name (insert real name)".
+*/
+
+using System;
+
+namespace TechProj3A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter your Name.");
+            // This variable will gather data from user input
+            string name = Console.ReadLine();
+            // Adds a line break
+            Console.WriteLine();
+            // Console's response
+            Console.WriteLine("Hello" + " " + name);
+        } // End of Main
+    } // End of class
+} // End of namespace
+
+/* 
+   Hello professor, I apologize if this was not how we were supposed to do this.
+   I watched Clinton's video and was unable to get the custom methods to work in the console.
+   This does however achieve the goal of the console responding with the users name.
+*/
